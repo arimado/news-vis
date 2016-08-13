9044405a6e8c2a1f2bf5a207311e7a0ca52d88ac
doloreel <jarimado@gmail.com>
2016-08-13 18:04:59 +1000

    showing table from example

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
 console.log('main.js is now loaded')
+
+d3.json("../posts.json", function(err, posts) {
+
+    var data = posts.data.children;
+
+    data.forEach(function(d) {
+        d.data.created *= 1000;
+    })
+
+    console.log(data);
+
+    var display = d3.select("#display")
+    var tdiv = display.append("div").classed("table", true)
+    var table = d3.chart.table()
+    table.data(data)
+    console.log(table);
+    table(tdiv)
+
+
+})
